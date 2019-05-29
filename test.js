05e05a326818b7fbc95cb66d9f62f6312af06267
k4m4 <nikolaskam@gmail.com>
2019-05-30 02:33:02 +0300

    Fix test onion addresses

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 import test from 'ava';
 import m from '.';
 
-const onions = ['https://abcdefghijkl1234.onion/', 'http://xmh57jrzrnw6insl.onion/', 'https://facebookcorewwwi.onion/'];
+const onions = ['https://abcdefghijkl1234.onion/', 'https://3g2upl4pq6kufc4m.onion/', 'https://facebookcorewwwi.onion/'];
 
 test('Active onion links', async t => {
 	const activeOnions = await m(onions);
